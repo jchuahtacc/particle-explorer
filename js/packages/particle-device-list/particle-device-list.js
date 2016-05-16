e57941858bdeff54252b75d2f0af394bfbaa60a9
Joon Yee Chuah <jchuah@tacc.utexas.edu>
2016-05-16 16:32:52 -0500

    device list json loading

--- js/packages/particle-device-list/particle-device-list.js
+++ js/packages/particle-device-list/particle-device-list.js
@@ -12,8 +12,29 @@ define('particledevicelist',
     function fileSelectCallback(e) {
       var reader = new FileReader();
       reader.onload = function(e) {
+        var ok = true;
         var contents = e.target.result;
-        console.log(contents);
+        try {
+          data = JSON.parse(contents);
+        } catch(e) {
+          $.bootstrapGrowl("Invalid JSON data", {type : "warning"});
+          ok = false;
+        }
+
+        if (ok) {
+          $.bootstrapGrowl("JSON data loaded", {type : "success"});
+          if (data.devices) {
+            for (var key in data.devices) {
+              var device = data.devices[key];
+              if (device && device.deviceId && device.accessToken) {
+                var newDevice = $(listItemHTML);
+                newDevice.find('[particle-device-listitem="description"]').html(device.deviceId + " <i>(" + device.accessToken + ")</i>" );
+                newDevice.appendTo('[particle-device-list="deviceList"]');
+              }
+            }
+          }
+          // load list from file
+        }
       }
       reader.readAsText(e.target.files[0]);
     }
